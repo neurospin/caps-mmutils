8f2625866240b58b300fe60d328cdd2975bc92a6
dgoyard <dg240989@is226572.intra.cea.fr>
2015-09-02 09:07:30 +0200

    test_mmutils: using unittest methods

--- mmutils/test/test_mmutils.py
+++ mmutils/test/test_mmutils.py
@@ -29,14 +29,59 @@ class TestUtils(unittest.TestCase):
     def test_specific_functions(self):
         """ Test functions execution follow up.
         """
-        element_to_list("element_test")
-        list_to_element(["singleton_element"])
-        ungzip_file(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                    "test_file.gz"),
-                    output_directory=self.outdir)
-        gzip_file(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                  "test_file.txt"),
-                  output_directory=self.outdir)
+        # element_to_list tests
+        out_list = element_to_list("element_test")
+        self.assertIsInstance(out_list, list)
+        self.assertEqual(out_list, ["element_test"])
+
+        # list_to_element tests
+        with self.assertRaises(ValueError):
+            list_to_element([1, 2, 3])
+        with self.assertRaises(ValueError):
+            list_to_element("no_list")
+        out_element = list_to_element(["singleton_element"])
+        self.assertIn(out_element, ["singleton_element"])
+        self.assertIsInstance(out_element, str)
+
+        # ungzip_file tests
+        out_file = ungzip_file(os.path.join(os.path.dirname(
+            os.path.realpath(__file__)),
+            "test_file.gz"),
+            output_directory=self.outdir,
+            prefix="u")
+        self.assertTrue(os.path.isfile(out_file))
+        self.assertEqual("u", os.path.basename(out_file)[0])
+
+        with self.assertRaises(ValueError):
+            ungzip_file("no_file",
+                        output_directory=self.outdir,
+                        prefix="u")
+        with self.assertRaises(ValueError):
+            ungzip_file(os.path.join(os.path.dirname(
+                os.path.realpath(__file__)),
+                "test_file.gz"),
+                output_directory="non_existent",
+                prefix="u")
+
+        # gzip file tests
+        out_file = gzip_file(os.path.join(os.path.dirname(
+            os.path.realpath(__file__)),
+            "test_file.txt"),
+            output_directory=self.outdir,
+            prefix="g")
+        self.assertTrue(os.path.isfile(out_file))
+        self.assertEqual("g", os.path.basename(out_file)[0])
+
+        with self.assertRaises(ValueError):
+            ungzip_file("no_file",
+                        output_directory=self.outdir,
+                        prefix="g")
+        with self.assertRaises(ValueError):
+            ungzip_file(os.path.join(os.path.dirname(
+                os.path.realpath(__file__)),
+                "test_file.gz"),
+                output_directory="non_existent",
+                prefix="g")
 
 
 def test():
